a20f048c105f95a0e79ec9841fae9ec0f659b3bf
hoba <hoba@ultracombos.com>
2016-03-04 22:06:21 +0800

    update to newest freenect2 add example vs files use lib instead of compile cpp refine code

--- libs/libfreenect2/include/libfreenect2/frame_listener.hpp
+++ libs/libfreenect2/include/libfreenect2/frame_listener.hpp
@@ -36,30 +36,63 @@
 namespace libfreenect2
 {
 
-/** A frame from the stream. */
-class  Frame
+/** @defgroup frame Frame Listeners
+ * Receive decoded image frames, and the frame format.
+ */
+
+/** Frame format and metadata. @ingroup frame */
+class LIBFREENECT2_API Frame
 {
   public:
   /** Available types of frames. */
   enum Type
   {
-    Color = 1, ///< RGB frame.
-    Ir = 2,    ///< IR frame.
-    Depth = 4  ///< Depth frame.
+    Color = 1, ///< 1920x1080 32-bit BGRX.
+    Ir = 2,    ///< 512x424 float. Range is [0.0, 65535.0].
+    Depth = 4  ///< 512x424 float, unit: millimeter. Non-positive, NaN, and infinity are invalid or missing data.
   };
 
-  uint32_t timestamp;
-  uint32_t sequence;
+  /** (Proposed for 0.2) Pixel format. */
+  enum Format
+  {
+    BGRX,
+    RGBX,
+    Gray,
+    Float,
+    // Note that if format is 'raw' then 'bytes_per_pixel' actually contains the number of bytes
+    Raw
+  };
+
   size_t width;           ///< Length of a line (in pixels).
   size_t height;          ///< Number of lines in the frame.
-  size_t bytes_per_pixel; ///< Number of bytes in a pixel.
-  unsigned char* data;    ///< Data of the frame (aligned).
+  size_t bytes_per_pixel; ///< Number of bytes in a pixel. If frame format is 'Raw' this is the buffer size.
+  unsigned char* data;    ///< Data of the frame (aligned). @see See Frame::Type for pixel format.
+  uint32_t timestamp;     ///< Unit: roughly or exactly 0.1 millisecond
+  uint32_t sequence;      ///< Increasing frame sequence number
+  float exposure;         ///< From 0.5 (very bright) to ~60.0 (fully covered)
+  float gain;             ///< From 1.0 (bright) to 1.5 (covered)
+  float gamma;            ///< From 1.0 (bright) to 6.4 (covered)
+  uint32_t status;        ///< Reserved. To be defined in 0.2.
+  Format format;          ///< Reserved. To be defined in 0.2.
 
-  Frame(size_t width, size_t height, size_t bytes_per_pixel) :
+  /** Construct a new frame.
+   * @param width Width in pixel
+   * @param height Height in pixel
+   * @param bytes_per_pixel Bytes per pixel
+   * @param data_ Memory to store frame data. If `NULL`, new memory is allocated.
+   */
+  Frame(size_t width, size_t height, size_t bytes_per_pixel, unsigned char *data_ = NULL) :
     width(width),
     height(height),
-    bytes_per_pixel(bytes_per_pixel)
+    bytes_per_pixel(bytes_per_pixel),
+    data(data_),
+    exposure(0.f),
+    gain(0.f),
+    gamma(0.f),
+    rawdata(NULL)
   {
+    if (data_)
+      return;
     const size_t alignment = 64;
     size_t space = width * height * bytes_per_pixel + alignment;
     rawdata = new unsigned char[space];
@@ -68,7 +101,7 @@ class  Frame
     data = reinterpret_cast<unsigned char *>(aligned);
   }
 
-  ~Frame()
+  virtual ~Frame()
   {
     delete[] rawdata;
   }
@@ -77,16 +110,19 @@ class  Frame
   unsigned char* rawdata; ///< Unaligned start of #data.
 };
 
-/** Callback class for waiting on a new frame. */
-class  FrameListener
+/** Callback interface to receive new frames. @ingroup frame
+ * You can inherit from FrameListener and define your own listener.
+ */
+class LIBFREENECT2_API FrameListener
 {
 public:
   virtual ~FrameListener();
 
   /**
-   * A new frame has arrived, process it.
+   * libfreenect2 calls this function when a new frame is decoded.
    * @param type Type of the new frame.
    * @param frame Data of the frame.
+   * @return true if you want to take ownership of the frame, i.e. reuse/delete it. Will be reused/deleted by caller otherwise.
    */
   virtual bool onNewFrame(Frame::Type type, Frame *frame) = 0;
 };
--- libs/protonect/ofProtonect.h
+++ libs/protonect/ofProtonect.h
@@ -12,42 +12,42 @@
 #include <libfreenect2/logger.h>
 #include <transfer_pool.h>
 #include <event_loop.h>
-#include "ofRGBPacketProcessor.h"
+//#include "ofRGBPacketProcessor.h"
 
-#include "ofAppGLFWWindow.h"
-#include "ofAppRunner.h"
+//#include "ofAppGLFWWindow.h"
+//#include "ofAppRunner.h"
 
-class ofProtonect{
+class ofProtonect {
 
-    public:
+public:
 
 
-    
-        ofProtonect();
-    
-        int openKinect(std::string serialNo);
-        void updateKinect(ofPixels & rgbPixels, ofFloatPixels & depthPixels);
-        int closeKinect();
-    
-        libfreenect2::Freenect2 & getFreenect2Instance(){
-            return freenect2;
-        }
-  
-    protected:
-  
-        bool bOpened;
-        
-        libfreenect2::Freenect2 freenect2;
 
-        libfreenect2::Freenect2Device *dev = 0;
-        libfreenect2::PacketPipeline *pipeline = 0;
+	ofProtonect();
 
-        libfreenect2::FrameMap frames;
+	int openKinect(std::string serialNo);
+	void updateKinect(ofPixels & rgbPixels, ofFloatPixels & depthPixels);
+	int closeKinect();
 
-        libfreenect2::Registration* registration;
-        libfreenect2::SyncMultiFrameListener * listener;
-        libfreenect2::Frame  * undistorted = NULL;
-        libfreenect2::Frame  * registered = NULL;
-        libfreenect2::Frame  * bigFrame = NULL;
+	libfreenect2::Freenect2 & getFreenect2Instance() {
+		return freenect2;
+	}
+
+protected:
+
+	bool bOpened;
+
+	libfreenect2::Freenect2 freenect2;
+
+	libfreenect2::Freenect2Device *dev = 0;
+	libfreenect2::PacketPipeline *pipeline = 0;
+
+	libfreenect2::FrameMap frames;
+
+	libfreenect2::Registration* registration;
+	libfreenect2::SyncMultiFrameListener * listener;
+	libfreenect2::Frame  * undistorted = NULL;
+	libfreenect2::Frame  * registered = NULL;
+	libfreenect2::Frame  * bigFrame = NULL;
 
 };
